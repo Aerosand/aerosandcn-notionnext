d49f04dc79b92f6e392b3ef45c4c6774abc9105d
MiozLife <mioz.life@outlook.com>
2025-03-18 14:52:25 +0900

    Update PostHeader.js

--- themes/heo/components/PostHeader.js
+++ themes/heo/components/PostHeader.js
@@ -46,10 +46,12 @@ export default function PostHeader({ post, siteInfo, isDarkMode }) {
             // filter: 'blur(15px)'
             filter: 'blur(5px)'
           }}
-          className='coverdiv lg:opacity-50 lg:translate-x-96 lg:rotate-12'>
+          // className='coverdiv lg:opacity-50 lg:translate-x-96 lg:rotate-12'>
+          className='coverdiv lg:opacity-100'>
           <LazyImage
             id='post-cover'
-            className='w-full h-full object-cover max-h-[50rem] min-w-[50vw] min-h-[20rem]'
+            // className='w-full h-full object-cover max-h-[50rem] min-w-[50vw] min-h-[20rem]'
+            className='w-full h-full object-cover'
             src={headerImage}
           />
         </div>
