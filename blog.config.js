9c96db70366c1299bc56d84018f51d5e0304d65e
Aerosand <aerosand@outlook.com>
2025-07-22 21:06:55 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
-    '22bd34b07c4c80d9aa92fe108915eec7',
+    '22bd34b07c4c80d9aa92fe108915eec7,docs:238d34b07c4c80e7ba5bea8036a37cdc',
   THEME: process.env.NEXT_PUBLIC_THEME || 'magzine', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2022, // e.g if leave this empty, current year will be used.
